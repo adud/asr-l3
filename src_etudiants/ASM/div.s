e34e958a383086d139da32b17a7a2ecc4bb2768d
adud <antonin.dudermel@club.fr>
2017-11-02 12:33:55 +0100

    les indications demandées par le sujet...

--- src_etudiants/ASM/div.s
+++ src_etudiants/ASM/div.s
@@ -1,3 +1,7 @@
+	;;Division
+	;;si r0=A r1=B
+	;; apres div: r2=A//B r3=A%B
+
 	leti r0 19886
 	leti r1 212
 	call div
@@ -8,13 +12,13 @@ div:
 	let r3 r0
 	let r4 r1
 
-ind:	cmp r0 r4		
-	jumpif le ectr
+ind:	cmp r0 r4		;44bits lus
+	jumpif le ectr		;par boucle
 	shift left r4 1
-	jump ind
+	jump ind		
 
 ectr:	cmp r3 r1	;inv: r3 + r1*r2 
-	jumpif lt endiv
+	jumpif lt endiv	;nb bits lus<=108
 	shift right r4 1
 	shift left r2 1
 	sub3 r5 r3 r4		
--- src_etudiants/ASM/mult.s
+++ src_etudiants/ASM/mult.s
@@ -1,11 +1,14 @@
+	;;Multiplication :
+	;; si avant appel r0=A,r1=B
+	;; apres appel r2=A*B
 	leti r0 1928
 	leti r1 2893
 	call mult
 loop:	jump loop
 
 mult:	leti r2 0
-wb:	shift right r0 1
-	jumpif nc sk
+wb:	shift right r0 1	;inv:r0*r1+r2
+	jumpif nc sk		;nb bits lus<=69
 	add2 r2 r1
 sk:	shift left r1 1
 	cmpi r0 0
