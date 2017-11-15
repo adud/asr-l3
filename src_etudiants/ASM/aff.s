6f54a69d796495ab6222123fe2dd82aeae3ce902
adud <antonin.dudermel@club.fr>
2017-11-15 08:49:15 +0100

    ajout de putchar demandé par le prof (basé sur la bibli graphique)

--- src_etudiants/ASM/aff.s
+++ src_etudiants/ASM/aff.s
@@ -1,20 +1,30 @@
-	leti r0 0x68000
-	setctr a1 r0
-	leti r0 0x10000
-	setctr a0 r0
-	setctr sp r0		;initstack
+	;; leti r0 0x68000
+	;; setctr a1 r0
+	;; leti r0 0x10000
+	;; setctr a0 r0
+	;; setctr sp r0		;initstack
 	
 
-	leti r0 0b0000000000000000
-	leti r1 0b0000000000111110
+	;; leti r0 0b0000000000000000
+	;; leti r1 0b0000000000111110
+	
+	;; call write
 
-	call write
+	leti r0 0x10000
+	setctr sp r0
+
+	leti r0 -1
+	leti r1 0
+	leti r2 0
+	leti r3 47
+
+	call putchar
 
 loop:	jump loop
 	
 	;; Ecrire un texte :
 	;; a0 pointe vers l'ecran (est le crayon)
-	;; a1 pointe vers la place du mot en memoire
+	;; a1 pointe vers le début de la chaine de car en memoire
 	;; r0 couleur de fond
 	;; r1 couleur du texte
 
@@ -85,10 +95,11 @@ out:	pop r6
 	pop r3
 	pop r2
 	return
-	
-prchr:	;; va ecrire le code ascii de r2
+
+	;; FINAL
+	;;va ecrire le code ascii de r2
 	;; couleur r0 fond r1
-	getctr a1 r5
+prchr:	getctr a1 r5		
 	shift left r2 3
 	add2i r2 0x60000
 	setctr a1 r2
@@ -126,5 +137,19 @@ fi:
 	setctr a1 r5
 	return
 
+putchar:
+	let r4 r1	;repondre aux spec du prof
+	shift left r4 2
+	add2 r4 r1
+	shift left r4 5
+	add2 r4 r2
+	shift left r4 4
+	add2i r4 0x10000
+	setctr a0 r4
+	let r1 r0
+	leti r0 0
+	let r2 r3
+	call prchr
+	return
 #include div.s
 #endmain
