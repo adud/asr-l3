529303312bf7673273b2239be316fb2da01d2b6c
adud <antonin.dudermel@club.fr>
2017-11-17 12:13:41 +0100

    on continue le tp 1

--- tp1-vhdl/clock.vhdl
+++ tp1-vhdl/clock.vhdl
@@ -9,7 +9,7 @@ end entity;
 
 
 architecture behaviorial of clock_generator is
-   constant clock_period : time := 1 ns;
+   constant clk_period : time := 1 ns;
 
    begin
    clock_process :process
@@ -21,3 +21,47 @@ architecture behaviorial of clock_generator is
    end process;
 
 end;
+
+
+library ieee; 
+use ieee.std_logic_1164.all;
+library work;
+
+entity registre is
+  port(clk, enable, d, reset : in std_logic;
+       q : out std_logic );
+end entity;
+
+
+architecture behaviorial of registre is
+begin
+  reg_process : process(clk)
+  begin
+    if rising_edge(clk) then
+      if reset = '1' then
+        q <= '0';
+      else
+        q <= d;
+      end if;
+    end if;
+  end process;
+end;
+
+library ieee; 
+use ieee.std_logic_1164.all;
+library work;
+
+entity Reg_N_Reset is
+  generic (n : integer);
+  port(clk,enable,reset : in std_logic;
+       d : in std_logic_vector(n-1 downto 0);
+       q : out std_logic_vector(n-1 downto 0)
+       );
+end entity;
+
+architecture rtl of Reg_N_Reset is
+  component registre is
+      port(clk, enable, d, reset : in std_logic;
+       q : out std_logic );
+  end component;
+  
