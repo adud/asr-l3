bbaeebdb9af4716da76686d64222bd9b4fdc6ff0
adud <antonin.dudermel@club.fr>
2017-11-25 00:08:37 +0100

    déplacement des fichiers pour laisser la place aux entrées clavier

--- src_etudiants/simu.src/screen.h
+++ src_etudiants/simu.src/screen.h
@@ -6,7 +6,8 @@ const int WIDTH = 160;
 const int HEIGHT = 128;
 const int MEM_SCREEN_BEGIN = 0x10000;// -> 0x60000
 //de la place pour un aff ascii 0x60000 -> 0x62000
-const int MEM_KBD_BEGIN = 0x10000; 
+const int MEM_KBD_BEGIN = 0x62000;
+//la reception du clavier 0x6200 -> 0x6280
 
 #include <SDL2/SDL.h>
 #include <stdio.h>
