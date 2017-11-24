9066861209ae5e8701ed9b606f0aa669c7961a4b
alban <alban.reynaud@laposte.net>
2017-11-24 08:06:41 +0100

    Les coordonnées x et y étaient inversées, correction. Correction de l'algorithme de tracé de droite (algorithme disponible seulement pour une certaine pente de droite).

--- src_etudiants/asm.src/graphic.s
+++ src_etudiants/asm.src/graphic.s
@@ -1,11 +1,13 @@
     leti r0 0x10000
     setctr sp r0 ; initialisiation du pointeur de pile
+    leti r1 10
+    leti r2 10
+    leti r3 90
+    leti r4 50
+    leti r0 0b111110000000000
+    call fill
     leti r0 0b11111
-    leti r1 0
-    leti r2 0
-    leti r3 80
-    leti r4 40
-    call plot
+    call draw
     end: jump end
 #main
 clear_screen:
@@ -25,30 +27,29 @@ cls_loop:
     return
 plot:
     ; r0 contient la couleur du pixel que l'on va afficher
-    ; r1 contient la ligne (0 en haut, 128 en bas) du pixel
-    ; r2 contient la colonne (0 à gauche, 159 à droite) du pixel
+    ; need some comment about r1
+    ; need some comment about r2
     push r1
     push r2
     push r3
     getctr a0 r3
     push r3
-    ; Les lignes sont "inversées" par rapport à ce que fait la SDL. Ainsi,
-    ; on commence par effectuer r1 <- 127 - r1
-    xor3i r1 r1 127
+    ; need some comment about this operation
+    xor3i r2 r2 127
 
-    ; on multiplie r1 par 160 et on l'ajoute à r2
-    let r3 r1
-    shift left r1 2
-    add2 r1 r3
-    shift left r1 5
-    add2 r2 r1
+    ; on multiplie r? par 160 et on l'ajoute à r?
+    let r3 r2
+    shift left r2 2
+    add2 r2 r3
+    shift left r2 5
+    add2 r1 r2
 
-    ; on multiplie r2 par 16 et on ajoute 0x10000
-    shift left r2 4
-    add2i r2 0x10000
+    ; on multiplie r? par 16 et on ajoute 0x10000
+    shift left r1 4
+    add2i r1 0x10000
 
-    ; et on affiche le pixel d'adresse r2 :
-    setctr a0 r2
+    ; et on affiche le pixel d'adresse r? :
+    setctr a0 r1
     write a0 16 r0
     pop r3
     setctr a0 r3
@@ -59,50 +60,50 @@ plot:
 fill:
     push r1
     push r2
-    push r3
+    push r4
     push r5
     push r6
     push r7
     getctr a0 r5
     push r5
-    let r7 r2
+    let r7 r1
     ; de même que pour plot, on effectue les opérations :
-    ; r1 <- 127 - r1
-    ; r3 <- 127 - r3
-    ; ainsi, puisque r1 ≤ r3 est supposé vrai à l'entrée de l'algorithme, on
-    ; a désormais r1 ≥ r3.
-    xor3i r1 r1 127
-    xor3i r3 r3 127
+    ; r2 <- 127 - r2
+    ; r4 <- 127 - r4
+    ; ainsi, puisque r2 ≤ r4 est supposé vrai à l'entrée de l'algorithme, on
+    ; a désormais r2 ≥ r4.
+    xor3i r2 r2 127
+    xor3i r4 r4 127
 
-    ; première « boucle for » : r1 varie par valeurs décroissantes jusqu'à r3.
+    ; première « boucle for » : r2 varie par valeurs décroissantes jusqu'à r4.
 fill_iter_raw:
-    cmp r1 r3
+    cmp r2 r4
     jumpif lt fill_iter_raw_exit
 
-    ; r5 <- 160*r1
-    let r5 r1
+    ; r5 <- 160*r2
+    let r5 r2
     shift left r5 2
-    add2 r5 r1
+    add2 r5 r2
     shift left r5 5
 
-    ; deuxième « boucle for » : r2 varie par valeurs décroissantes jusqu'à r4.
+    ; deuxième « boucle for » : r1 varie par valeurs décroissantes jusqu'à r3.
 fill_iter_col:
-    cmp r2 r4
+    cmp r1 r3
     jumpif gt fill_iter_col_exit
 
-    ; r6 <- 16*(r5 + r2) + 0x10000
+    ; r6 <- 16*(r5 + r1) + 0x10000
     let r6 r5
-    add2 r6 r2
+    add2 r6 r1
     shift left r6 4 
     add2i r6 0x10000
     setctr a0 r6
     write a0 16 r0
-    add2i r2 1
+    add2i r1 1
     jump fill_iter_col
 
 fill_iter_col_exit:
-    let r2 r7
-    sub2i r1 1
+    let r1 r7
+    sub2i r2 1
     jump fill_iter_raw
 
 fill_iter_raw_exit:
@@ -111,14 +112,14 @@ fill_iter_raw_exit:
     pop r7
     pop r6
     pop r5
-    pop r3
+    pop r4
     pop r2
     pop r1
     return
 draw:
     ;; r1, r2, r3, r4 contiennent x1, y1, x2, y2
     ;; on cherche à tracer la droite allant de (x1, y1) à (x2, y2)
-    ;; on suppose ici que x1 ≤ x2, y1 ≤ y2 et que y2 - y1 ≤ x2 - x1
+    ;; on suppose ici que y1 ≤ y2 et que |y2 - y1| ≤ x2 - x1
     push r1
     push r2
     push r4
@@ -126,11 +127,12 @@ draw:
     push r6
     push r7
 
-    sub3 r5 r3 r1 ; r5 contient dx = x2 - x1
-    sub2 r4 r2
-    shift left r4 1 ; r4 contient dy = 2 * (y2 - y1)
-    leti r6 0
-draw_loop:
+    sub3 r5 r3 r1
+    shift left r5 1 ; r5 contient 2 * dy
+    sub3 r6 r1 r3   ; r6 contient -dy
+    sub2 r4 r2      ; r4 contient 2 * dx
+    shift left r4 1
+draw_loop1:
     call plot
     ; on incrémente x, et on termine la boucle si x atteint sa valeur max.
     add2i r1 1 
@@ -138,13 +140,29 @@ draw_loop:
     jumpif gt draw_endloop
 
     add2 r6 r4
-    cmp r6 r3
-    jumpif lt draw_loop
-    ;; si r6 >= r3, 
+    cmpi r6 0
+    jumpif slt draw_loop1
+    ;; si r6 >= r5
     add2i r2 1
-    sub2 r6 r3
-    sub2 r6 r3
-    jump draw_loop
+    sub2 r6 r5
+    jump draw_loop1
+
+;draw_loop2:
+;    call plot
+;
+;    add2i r1 1 
+;    cmp r1 r3
+;    jumpif gt draw_endloop
+
+;    add2 r6 r4
+;    cmp r6 r5
+;    jumpif gt draw_loop2
+;    ;; si r6 <= r5, 
+;    sub2i r2 1
+;    add2 r6 r5
+;    add2 r6 r5
+;    jump draw_loop2
+
 draw_endloop:
     pop r7
     pop r6
