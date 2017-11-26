cf81bd8c9a0c22152e249540651170d641694ac4
adud <antonin.dudermel@club.fr>
2017-11-26 12:36:59 +0100

    changements mineurs : convention de maintien de registres

--- src_etudiants/asm.src/mult.s
+++ src_etudiants/asm.src/mult.s
@@ -7,12 +7,16 @@
 loop:	jump loop
 
 #main ; le code intéressant commence à partir de maintenant
-mult:	leti r2 0
+mult:	push r0
+	push r1
+	leti r2 0
 wb:	shift right r0 1	;inv:r0*r1+r2
 	jumpif nc sk		;nb bits lus<=69
 	add2 r2 r1
 sk:	shift left r1 1
 	cmpi r0 0
 	jumpif nz wb
+	pop r1
+	pop r0
 	return
 #endmain
