f1652f791624229d29fba5295e528802c5a7cd7a
adud <antonin.dudermel@club.fr>
2017-12-19 19:08:51 +0100

    fin de la multiplication à virgule fixe, TODO attaquer la division

--- src_etudiants/asm.src/fxrth16.s
+++ src_etudiants/asm.src/fxrth16.s
@@ -2,13 +2,12 @@
 	;; a virgule fixes
 	;; 16bits avant la virgule 16 bits apres
 
+	leti r1 0x5555
 	leti r0 3
 	call int2fix
-	let r1 r0
-	leti r0 1
-	call int2fix
 	call multfx
-
+	let r0 r2
+	call fix2int
 loop:	jump loop
 	
 #main
@@ -31,8 +30,9 @@ fix2int:
 	add2i r0 1	
 end:	return
 
+	;; la partie deucimale d'un nombre
 radix:
-	xor3i r0 r0 0x1111
+	and2i r0 0x1111
 	return
 
 	;; multiplie r0 r1, deux vfixe, pas de gestion d'overflow
@@ -41,20 +41,18 @@ radix:
 	;; {r2=A*B}
 	;; 
 	;; Philippe <3
-	;; r6 conserve
-multfx:	
+	;; pas de registre conserve
+multfx:
+	push r7
+	call mult16.s$mult32
+	shift left r2 16
+	shift right r3 16
+	add2 r2 r3
+	pop r7
 	return
 
-divfx:	let r2 r0
-	let r0 r1
-	push r7
-	call fix2int
-	pop r7
-	let r1 r0
-	let r0 r2
-	push r7
-	call div.s$div
-	pop r7
-	return
+divfx:	return
+	
+
 
 #endmain
