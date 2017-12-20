4367080f9e215e658155848bad63710d71d92a87
adud <antonin.dudermel@club.fr>
2017-12-20 09:19:46 +0100

    quelques commentaires erronés (la gauche, la droite tout ça)

--- src_etudiants/asm.src/div.s
+++ src_etudiants/asm.src/div.s
@@ -18,13 +18,13 @@ shiftl: ; on décale r3 vers la gauche jusqu'à que r3 soit plus grand que r0.
 	cmp r0 r3		;par boucle
 	jumpif ge shiftl
 mainloop:
-	shift right r3 1 ; par la suite, on décale r3 vers la gauche à chaque tour
+	shift right r3 1 ; par la suite, on décale r3 vers la droite à chaque tour
 	shift left r2 1	 ;inv : r0+r1*r2
 	cmp r0 r3	 ;inv : r0 < r3
 	jumpif lt cond	 ;nb bits lus <93
 	sub2 r0 r3
 	add2i r2 1
-cond: ; On s'arrête quand r3 vaut de r1.
+cond: ; On s'arrête quand r3 vaut moins de r1.
       ; Le bloc cond vérifie cette condition.
 	cmp r1 r3
 	jumpif neq mainloop
