b4df516502be0e12d70a31b63f4802d6838d2004
adud <antonin.dudermel@club.fr>
2017-12-27 18:43:18 +0100

    un dessin de cube effectif, plus qu'a trouver comment l'effacer

--- src_etudiants/asm.src/pluie.s
+++ src_etudiants/asm.src/pluie.s
@@ -1,31 +1,34 @@
-	leti r0 500
+	leti r0 100
+	;; un petit test graphique
+
 forloop:
 	push r0
 	call fxrth16.s$int2fix
-	call coords_square_dist
-	let r5 r1
-	let r1 r0
-	let r2 r0
-	let r3 r0
-	let r4 r0
-	add2 r3 r5
-	add2 r4 r5
-	leti r0 -1
-	call graphic.s$fill
-	leti r0 20
-	call attact.s$pause
-	leti r0 0
-	call graphic.s$fill
+	leti r1 0
+	leti r2 0
+
+	leti r3 0x3c0
+	call drawcube
+	;; call graphic.s$clear_screen
+	
+	;; leti r0 10
+	;; call attact.s$pause
+	;; leti r0 0		
+	;; call graphic.s$clear_screen
+	;; leti r0 10
+	;; call attact.s$pause
 	pop r0
-	sub2i r0 1
+	sub2i r0 0x1
 	jumpif nz forloop
 	
 loop:	jump loop
+
 	
 .main
 .include fxrth16.s
 .include attact.s
-.include graphic.s 
+.include graphic.s
+.include mult.s
 
 	;; coords_square_dist
 	
@@ -34,44 +37,145 @@ loop:	jump loop
 	;; after coords_square_dist :
 	;; r1 contains int b : the value of the two coords of
 	;; bottom left corner of the bottom left square
-	;; r2 contains int a-b : the length of a square
+	;; r2 contains int l : the length of a square
 	;; see ggb file
 
 	;; ds will be fixed as 73.0 until we have macros
 	;; K as 42
+
+	;; works fine until r0=0x6180000 then calculation overflow
 coords_square_dist:
 	push r7
 	let r2 r0		;r2 is db
-	add2i r0 0x490000
+	add2i r0 0x490000	;79.0 = ds
 	shift left r0 1 
 	push r0			;2*(db+ds) on the stack
 	
-	leti r0 42		;42 means K
-	call fxrth16.s$int2fix
-	let r1 r0
+	leti r1 0x2a0000	;0x2a0000 means K as float
 	let r0 r2
 	call fxrth16.s$multfix
 	pop r1			;2*(db+ds)
 	let r0 r2
 	call fxrth16.s$divfix
 	push r2			;cdis on the stack
-	leti r0 3
-	call fxrth16.s$int2fix
+	leti r0 0x30000		;this 3.0
 	let r1 r2
 	call fxrth16.s$multfix	;r2 is b
 	pop r1			;cdis
 	push r2			;b on the stack
-	leti r0 2		
-	call fxrth16.s$int2fix
+	leti r0 0x20000		;2.0
 	call fxrth16.s$multfix
-	leti r0 42		;42 is K I *really* need macros
-	call fxrth16.s$int2fix
-	sub2 r0 r2
+	leti r0 0x2a0000	;42.0 is K I *really* need macros
+	sub2 r0 r2		;K - 2*cdis = l
 	call fxrth16.s$fix2int
 	let r1 r0
-	pop r0 			;b
+	pop r0			;b
 	call fxrth16.s$fix2int
 	pop r7
 	return
 
+	;; drawcube : if r0 contains the distance between cube & screen
+	;; r1 r2 its position on the screen
+	;; draws the cube with colour r3
+	
+drawcube:
+	push r7
+	push r3			;colour
+	push r2			;cy
+	push r1			;cx
+	push r0			;d
+	call coords_square_dist
+	pop r4			;d
+	push r0			;b
+	push r1			;l
+	leti r0 0x2a0000	;K float once again
+	add2 r0 r4
+	call coords_square_dist	;			         +----+
+	pop r6		;l					 | cy |	
+	pop r5		;b       reg :  b' l' . . . b l . stack: | cx |
+	;; free space : as coordinates <= 256, one can concatenate
+	;; four coords in the same register : ri = c3e24 + c2e16 + c1e8 + c0
+	;; avoid lots of pop & push
+	shift left r5 8
+	add2 r5 r0
+	shift left r6 8
+	add2 r6 r1		;reg : . . . . . be8+b' le8+l' . stack idem
+
+	let r4 r5
+	let r1 r6
+	pop r0			;cx
+	call mult.s$mult
+	add2 r5 r2
+	shift left r5 16
+	let r1 r6
+	pop r0			;cy ;empty stack
+	call mult.s$mult
+	add2 r5 r2
+	add2 r5 r4
+	;; now r5 contains all the coordinates :
+	;; dlf.x.e24 + dlb.x.e16 + dlf.y.e8 + dlb.y
+	;; where dlf is down left front
+	
+	;; r6 contains the length of the squares :
+	;; front_square_length.e8 + back_square_length
+	pop r0
+	
+	push r6
+	add2 r6 r5		;dl ul (down-left up-left)
+	call draw_3_vertices
+	let r5 r6
+	pop r6
+	push r6
+	shift left r6 16
+	add2 r6 r5		;ul ur
+	call draw_3_vertices
+	let r5 r6
+	pop r6
+	push r6
+	sub3 r6 r5 r6		;ur dr
+	call draw_3_vertices
+	let r5 r6
+	pop r6
+	shift left r6 16
+	sub3 r6 r5 r6
+	call draw_3_vertices
+
+	pop r7
+	return
+	
+	
+
+	
+	;; in draw_cube the 12 vertices are drawn by pack of 3
+	;; Draws the lines with the colour in r0
+	;; r5 & r6 are side squares of the cube
+draw_3_vertices:
+	push r7
+	;; depth line
+	and3i r1 r5 0xff000000
+	shift right r1 24
+	and3i r2 r5 0x0000ff00
+	shift right r2 8
+	and3i r3 r5 0x00ff0000
+	shift right r3 16
+	and3i r4 r5 0x000000ff
+	call graphic.s$draw
+	;; front line
+	and3i r3 r6 0xff000000
+	shift right r3 24
+	and3i r4 r6 0x0000ff00
+	shift right r4 8
+	call graphic.s$draw
+	;; back line
+	and3i r1 r5 0x00ff0000
+	shift right r1 16
+	and3i r2 r5 0x000000ff
+	and3i r3 r6 0x00ff0000
+	shift right r3 16
+	and3i r4 r6 0x000000ff
+	call graphic.s$draw
+
+	pop r7
+	return
+	
 .endmain
